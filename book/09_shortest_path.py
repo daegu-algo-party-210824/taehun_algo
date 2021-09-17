453b2bf90d075c05ab940f4e33588fe781acfe66
smarthun0106 <smarthun0106@example.com>
2021-09-17 11:55:07 +0900

    first commit

--- book/09_shortest_path.py
+++ book/09_shortest_path.py
@@ -16,24 +16,45 @@ graph = [
     [5, 6, 2],
 ]
 
-visited = [int(1e9)] * node
-visited[0] = 0
+visited = [int(1e9)] * (node+1)
+check = [False] * (node+1)
 
-# 각 노드 개수 계산
-def node_count():
-    node_count = [0]
+# count each node list
+node_count = [0]
+count = 0
+for i in range(1, node+1):
+    for g in graph:
+        if g[0] == i:
+            count += 1
+    node_count.append(count)
     count = 0
-    for i in range(1, node+1):
-        for g in graph:
-            if g[0] == i:
-                count += 1
-        node_count.append(count)
-        count = 0
-    return node_count
+
+
+# # find shortest path node
+def shortest_node(v, c):
+    print(v, c)
+    for i in range(0, len(c)):
+        if c[i] == True:
+            v.remove(v[i])
 
 
 
-print(node_count())
+# 노드별 방문 기록
+for i in range(1, node+1):
+    for ii in range(0, d_count):
+        if len(graph[ii]) == 1:
+            visited[graph[ii][0]] = 0
+
+        elif i == graph[ii][0]:
+            visited[graph[ii][1]] = graph[ii][2]
+    check[i] = True
+    min_node = shortest_node(visited, check)
+
+
+
+
+
+
 
 
 
