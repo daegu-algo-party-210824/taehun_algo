d124d2ca7f05cd467a2c56c79dab292aaa33978b
smarthun0106 <smarthun0106@example.com>
2021-09-17 10:53:39 +0900

    first commit

--- book/09_shortest_path.py
+++ book/09_shortest_path.py
@@ -1,7 +1,7 @@
 node = 6
 d_count = 11
 
-g = [
+graph = [
     [1],
     [1, 2, 2],
     [1, 3, 5],
@@ -17,3 +17,26 @@ g = [
 ]
 
 visited = [int(1e9)] * node
+visited[0] = 0
+
+# 각 노드 개수 계산
+def node_count():
+    node_count = [0]
+    count = 0
+    for i in range(1, node+1):
+        for g in graph:
+            if g[0] == i:
+                count += 1
+        node_count.append(count)
+        count = 0
+    return node_count
+
+
+
+print(node_count())
+
+
+
+
+# check_visited()
+print(visited)
